95e6de92f820682939a97477d00ddfaf9d551f85
SergeyKudrin84 <conan4@yandex.ru>
2026-01-28 14:19:19 +1000

    Выполненное задание Спринт 9

--- src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
+++ src/main/ru/practicum/moviehub/http/BaseHttpHandler.java
@@ -1,7 +1,56 @@
 package ru.practicum.moviehub.http;
 
+import com.google.gson.*;
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import ru.practicum.moviehub.api.ErrorResponse;
+import ru.practicum.moviehub.model.Movie;
+import ru.practicum.moviehub.store.MoviesStore;
+
+import javax.swing.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLDecoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class BaseHttpHandler implements HttpHandler {
+
+    protected static final String CT_JSON = "application/json; charset=UTF-8";
+    protected static final Charset UTF8 = StandardCharsets.UTF_8;
+
+    protected void sendJson(HttpExchange exchange, int statusCode, String json) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", CT_JSON);
+        exchange.sendResponseHeaders(statusCode, 0);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(json.getBytes(UTF8));
+        }
+    }
+
+    protected void sendNoContent(HttpExchange exchange, int statusCode) throws IOException {
+        exchange.getResponseHeaders().set("Content-Type", CT_JSON);
+        exchange.sendResponseHeaders(statusCode, -1);
+    }
+
+    protected void sendError(
+            HttpExchange exchange,
+            int statusCode,
+            ErrorResponse error) throws IOException {
+
+        Headers headers = exchange.getResponseHeaders();
+        headers.set("Content-Type", CT_JSON);
+        exchange.sendResponseHeaders(statusCode, 0);
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(new Gson().toJson(error).getBytes(UTF8));
+        }
+    }
 
 }
